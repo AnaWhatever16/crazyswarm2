372cef1dae0e28ef7468d96c532b52ad0861ade6
coppietersdm <matthieu.coppieters@student.uclouvain.be>
2024-04-10 14:55:39 +0200

    multiple trajectories in hello world

--- crazyflie_examples/crazyflie_examples/hello_world.py
+++ crazyflie_examples/crazyflie_examples/hello_world.py
@@ -1,22 +1,56 @@
 """Takeoff-hover-land for one CF. Useful to validate hardware config."""
 
+# from crazyflie_py import Crazyswarm
+
+
+# TAKEOFF_DURATION = 2.5
+# HOVER_DURATION = 5.0
+
+
+# def main():
+#     swarm = Crazyswarm()
+#     timeHelper = swarm.timeHelper
+#     cf = swarm.allcfs.crazyflies[0]
+
+#     cf.takeoff(targetHeight=1.0, duration=TAKEOFF_DURATION)
+#     timeHelper.sleep(TAKEOFF_DURATION + HOVER_DURATION)
+#     cf.land(targetHeight=0.04, duration=2.5)
+#     timeHelper.sleep(TAKEOFF_DURATION)
+
+
+# if __name__ == '__main__':
+#     main()
+
+
+"""Takeoff-hover-land for one CF. Useful to validate hardware config."""
+
 from crazyflie_py import Crazyswarm
+import numpy as np
+from math import cos, sin, pi
 
+TAKEOFF_DURATION = 1.0
+HOVER_DURATION = 1.0
 
-TAKEOFF_DURATION = 2.5
-HOVER_DURATION = 5.0
-
-
+def position(cf, pos):
+    vel = np.array([0.0, 0.0, 0.0])
+    acc = np.array([0.0, 0.0, 0.0])
+    yaw = 0.0
+    omega = np.array([0.0, 0.0, 0.0])
+    cf.cmdFullState(pos, vel, acc, yaw, omega)
+    
 def main():
     swarm = Crazyswarm()
     timeHelper = swarm.timeHelper
-    cf = swarm.allcfs.crazyflies[0]
 
-    cf.takeoff(targetHeight=1.0, duration=TAKEOFF_DURATION)
+    for cf in swarm.allcfs.crazyflies:
+        cf.takeoff(targetHeight=0.5, duration=TAKEOFF_DURATION)
     timeHelper.sleep(TAKEOFF_DURATION + HOVER_DURATION)
-    cf.land(targetHeight=0.04, duration=2.5)
-    timeHelper.sleep(TAKEOFF_DURATION)
 
+    for i in range(200):
+        for cf in swarm.allcfs.crazyflies:
+            position(cf, np.array([cos(i/10),sin(i/10),1.0])+cf.initialPosition)
+        timeHelper.sleep(0.1)
+        print('hello world')
 
 if __name__ == '__main__':
     main()
